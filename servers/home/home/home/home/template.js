2ec8d2050140c72dcb44e9710a605afcc9ab3dd5
zetra <karanpatadia28@gmail.com>
2025-08-10 01:04:55 +0530

    remvoing duplicate

--- servers/home/home/home/home/template.js
+++
@@ -1,9 +0,0 @@
-// servers/home/home/home/template.js
-async function main(ns) {
-  ns.tprint("Hello Remote API!");
-}
-export {
-  main
-};
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4vc2VydmVycy9ob21lL2hvbWUvaG9tZS9zZXJ2ZXJzL2hvbWUvc3JjL3RlbXBsYXRlLnRzIl0sInNvdXJjZVJvb3QiOiIvIiwibWFwcGluZ3MiOiI7QUFFQSxlQUFzQixLQUFLLElBQU07QUFDL0IsS0FBRyxPQUFPLG1CQUFtQjtBQUMvQjsiLCJuYW1lcyI6W119
--- servers/home/home/home/template.js
+++
@@ -1,9 +0,0 @@
-// servers/home/home/template.js
-async function main(ns) {
-  ns.tprint("Hello Remote API!");
-}
-export {
-  main
-};
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4vc2VydmVycy9ob21lL3NyYy90ZW1wbGF0ZS50cyJdLCJzb3VyY2VSb290IjoiLyIsIm1hcHBpbmdzIjoiO0FBRUEsZUFBc0IsS0FBSyxJQUFNO0FBQy9CLEtBQUcsT0FBTyxtQkFBbUI7QUFDL0I7IiwibmFtZXMiOltdfQ==
